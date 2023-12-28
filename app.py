02b0bcbfbda31829afdfc10226b9263c7eba236a
Cindy <seetheworld510306@gmail.com>
2023-12-29 00:03:43 +0800

    1001

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort, jsonify
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, FollowEvent, TextSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackAction
+from linebot.models import MessageEvent, TextMessage, FollowEvent, TextSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackAction, CarouselTemplate, CarouselColumn
 
 # Replace 'YOUR_CHANNEL_ACCESS_TOKEN' and 'YOUR_CHANNEL_SECRET' with your actual credentials
 line_bot_api = LineBotApi('dOBtG5dEqkb/JWk1UrLxfpeXWH44gdDUj0yW/8labLdLY3+1s1DxhTOQ+FM6u/DSh7bVatXM6SgikLni/98A7aCMiiefbFZ790FbIqHl7FG47vgsDBJBqo49IXfhnx1Y6AUzXzFnpysZq/qtmKXOfQdB04t89/1O/w1cDnyilFU=')
@@ -30,17 +30,20 @@ def handle_message(event):
     message_text = event.message.text
     if message_text == '廁所':
         content = tissuepaper()
-        # Send a button template
-        buttons_template = ButtonsTemplate(
-            title='Button Template',
-            text='想知道現在的衛生紙用量嗎！',
-            actions=[
-                PostbackAction(label='我要知道！', data='action=buy&itemid=1'),
-            ]
-        )
+        # Send a carousel template with image and button
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(
+                thumbnail_image_url='https://ibb.co/2jKjHPd',  # Replace with your image URL
+                title='想知道現在衛生紙的剩餘用量嗎！',
+                text='肯定要的吧',
+                actions=[
+                    PostbackAction(label='我要知道！', data='action=buy&itemid=1'),
+                ]
+            )
+        ])
         template_message = TemplateSendMessage(
-            alt_text='Button Template',
-            template=buttons_template
+            alt_text='Carousel Template',
+            template=carousel_template
         )
         line_bot_api.reply_message(event.reply_token, template_message)
 
