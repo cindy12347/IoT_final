d04345fe33c867f5b5aafd61310ef3815dc3594c
Cindy <seetheworld510306@gmail.com>
2023-12-29 00:51:56 +0800

    1229

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def handle_message(event):
         # Send a carousel template with image and button in the MessageEvent
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(
-                thumbnail_image_url='https://i.ibb.co/jL4LsGD/PDP-7-Prem-c8d51501-40f4-4c6f-9237-c520c11d8048-1120x1120.webp', 
+                thumbnail_image_url='', 
                 title='想知道衛生紙的剩餘用量嗎！',
                 text='肯定要的吧',
                 actions=[
