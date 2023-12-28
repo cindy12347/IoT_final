e2eeba60c3fad0386a6bc25cf85f4ab80391244c
Cindy <seetheworld510306@gmail.com>
2023-12-28 21:20:09 +0800

    2120

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, FollowEvent, TextSendMessage
 import requests
+import logging
 
 # Replace 'YOUR_CHANNEL_ACCESS_TOKEN' and 'YOUR_CHANNEL_SECRET' with your actual credentials
 line_bot_api = LineBotApi('dOBtG5dEqkb/JWk1UrLxfpeXWH44gdDUj0yW/8labLdLY3+1s1DxhTOQ+FM6u/DSh7bVatXM6SgikLni/98A7aCMiiefbFZ790FbIqHl7FG47vgsDBJBqo49IXfhnx1Y6AUzXzFnpysZq/qtmKXOfQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('c38f9d5ed556c8ebae0d0103dc89a738')
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.ERROR)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -19,6 +23,9 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+    except Exception as e:
+        logging.error(f"Error in callback: {e}")
+        return 'Internal Server Error', 500
 
     return 'OK'
 
@@ -39,12 +46,16 @@ def handle_follow(event):
 def new_api():
     # Example of making a request to another API (https://smart-campus.kits.tw/)
     api_url = "https://smart-campus.kits.tw/"
-    response = requests.get(api_url)
+    try:
+        response = requests.get(api_url)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        data = {"message": "This is a new API endpoint!", "status": "success", "api_data": response.text}
-    else:
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = {"message": "This is a new API endpoint!", "status": "success", "api_data": response.text}
+        else:
+            data = {"message": "Error accessing the external API", "status": "error"}
+    except Exception as e:
+        logging.error(f"Error in making API request: {e}")
         data = {"message": "Error accessing the external API", "status": "error"}
 
     return jsonify(data)
