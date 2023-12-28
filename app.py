9e016aa49679df80272e226ddf6e688f1adb33f4
Cindy <seetheworld510306@gmail.com>
2023-12-29 01:17:29 +0800

    1229

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, FollowEvent, TextSendMessage, TemplateSendMessage, PostbackEvent, PostbackAction, CarouselTemplate, CarouselColumn
+from linebot.models import MessageEvent, TextMessage, FollowEvent, TextSendMessage, TemplateSendMessage, PostbackAction, CarouselTemplate, CarouselColumn
+import requests
 
 # Replace 'YOUR_CHANNEL_ACCESS_TOKEN' and 'YOUR_CHANNEL_SECRET' with your actual credentials
 line_bot_api = LineBotApi('dOBtG5dEqkb/JWk1UrLxfpeXWH44gdDUj0yW/8labLdLY3+1s1DxhTOQ+FM6u/DSh7bVatXM6SgikLni/98A7aCMiiefbFZ790FbIqHl7FG47vgsDBJBqo49IXfhnx1Y6AUzXzFnpysZq/qtmKXOfQdB04t89/1O/w1cDnyilFU=')
@@ -24,39 +25,42 @@ def callback():
 @handler.add(FollowEvent)
 def handle_follow(event):
     user_id = event.source.user_id
-    # Send a carousel template with image and button in the FollowEvent
-    carousel_template = CarouselTemplate(columns=[
-        CarouselColumn(
-            thumbnail_image_url='https://i.ibb.co/jL4LsGD/PDP-7-Prem-c8d51501-40f4-4c6f-9237-c520c11d8048-1120x1120.webp', 
-            title='想知道衛生紙的剩餘用量嗎！',
-            text='肯定要的吧',
-            actions=[
-                PostbackAction(label='我要知道！', data='action=show_amount'),
-            ]
-        )
-    ])
-    template_message = TemplateSendMessage(
-        alt_text='衛生紙剩餘用量',
-        template=carousel_template
-    )
-    
-    line_bot_api.reply_message(event.reply_token, template_message)
+    content="welcome!"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_text = event.message.text
-    if message_text == '12345':
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Thanks for your response!"))
-    else:
-        # Handle other messages
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="I didn't understand that."))
+    if message_text == '廁所':
+        # Send a carousel template with image and button in the MessageEvent
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(
+                thumbnail_image_url='', 
+                title='想知道衛生紙的剩餘用量嗎！',
+                text='肯定要的吧',
+                actions=[PostbackAction(label='我要知道！', data='show_amount')]
+            )
+        ])
+        template_message = TemplateSendMessage(alt_text='衛生紙剩餘用量',template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
 
-@handler.add(PostbackEvent)
+@handler.add(PostbackAction)
 def handle_postback(event):
     data = event.postback.data
-    if data == 'action=show_amount':
+    if data == 'show_amount':
         # Respond with the result of left_cal when the user clicks "我要知道！"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Please type '12345' to confirm."))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="12345"))
+
+# New API endpoint
+@app.route("/new_api", methods=['GET'])
+def left_cal():
+    headers = {'token': 'your_token', 'Content-type': 'application/json'}
+    data1 = requests.get(url='', headers=headers)
+    data2 = requests.get(url='', headers=headers)
+
+    # Process the data and return the result
+    result = "234083234"  # Replace this with your logic for processing the data
+    return result
 
 if __name__ == "__main__":
     app.run()
