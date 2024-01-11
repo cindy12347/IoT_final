6cb402ace309fb90864e42aa8ad81e427103c216
Cindy <seetheworld510306@gmail.com>
2024-01-11 13:34:38 +0800

    1217

--- app.py
+++ app.py
@@ -93,16 +93,16 @@ def left_cal():
     response2 = requests.get(url='https://smart-campus.kits.tw/api/api/sensors_in_timeinterval/DISTANCE/7e8a1261-56a2-4ffd-ac2c-b7a5a1934422/1704907260000/1704907350000', headers=headers)
     
     # Parse JSON response
-    data2 = response2.json()
+    data = response2.json()
 
     # Extract values
-    values = [item["value"] for item in data2["Items"]]
+    values = [item["value"] for item in data["Items"]]
     
     # Convert values list to a string
     values_text = ', '.join(map(str, values))
 
 
-    return response1.text
+    return values_text
 
 if __name__ == "__main__":
     app.run()
